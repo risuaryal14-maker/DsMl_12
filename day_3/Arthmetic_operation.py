94d5af543881fe1f973026e3e2ccba3187b00273
risuaryal14 <risuaryal14@gmail.com>
2026-07-07 11:33:38 +0545

    latest update

---
+++ day_3/Arthmetic_operation.py
@@ -0,0 +1,51 @@
+"""ARITHMETIC OPERATIONS
+1. ADDITION-->ADD TWO OR MORE NUMBERS=>+
+2. SUBTRACTION-->SUBTRACT ONE NUMBER FROM ANOTHER=>-
+3. MULTIPLICATION-->MULTIPLY TWO OR MORE NUMBERS=>*
+4. DIVISION-->DIVIDE ONE NUMBER BY ANOTHER=>/
+5. FLOOR DIVISION-->DIVIDE TWO NUMBERS AND RETURN THE FLOOR OF THE RESULT=>//
+6. MODULUS-->RETURN THE REMAINDER OF A DIVISION=>%
+7. EXPONENTIATION-->RAISE A NUMBER TO A POWER=>**
+"""
+#ADDITION
+a=10
+b=5
+result=a+b
+print("Addition:", result)
+
+#SUBTRACTION
+a=10
+b=5
+result=a-b
+print("Subtraction:", result)
+
+#MULTIPLICATION
+a=10
+b=5
+result=a*b
+print("Multiplication:", result)
+
+#DIVISION
+a=10
+b=5
+result=a/b
+print("Division:", result)
+
+#FLOOR DIVISION
+a=10
+b=5
+result=a//b
+print("Floor Division:", result)
+
+#MODULUS
+a=10
+b=5
+result=a%b
+print("Modulus:", result)
+
+#EXPONENTIATION
+a=2
+b=3
+result=a**b
+print("Exponentiation:", result)
+
