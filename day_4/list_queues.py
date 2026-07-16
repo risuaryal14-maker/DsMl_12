f5bbb72b267156e8cb477e49ef6dc1ddd4c75acf
risuaryal14 <risuaryal14@gmail.com>
2026-07-16 22:17:13 +0545

    latest update

---
+++ day_4/list_queues.py
@@ -0,0 +1,8 @@
+ 
+#deque means Double Ended Queue
+from collections import deque
+queue = deque(["Eric", "John", "Michael"])
+queue.append("Terry") 
+queue.append("Graham") 
+queue.popleft()  
+print(queue)
