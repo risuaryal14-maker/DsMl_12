60eb3ba24e1dd9f51ffb610b834b0160e25e8a85
risuaryal14 <risuaryal14@gmail.com>
2026-07-16 22:21:31 +0545

    latest update

---
+++ Tasks/tasks_1.py
@@ -0,0 +1,16 @@
+'''
+1---Create a list of 5 favorite fruits. Add one more
+fruit using append() and print the updated list.
+
+2-----Create a list of numbers from 1 to 5. Remove
+the last item using pop() and print the final list.'''
+
+#question 1
+lst=['mango', 'banana', 'orange', 'grapes', 'kiwi']
+lst.append('pineapple')
+print(lst)
+
+#question 2
+lst2=[1, 2, 3, 4, 5]
+lst2.pop()
+print(lst2)
