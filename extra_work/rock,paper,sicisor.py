d76ab2ebb4a49c75763b3e58fd60cee565a785c0
risuaryal14 <risuaryal14@gmail.com>
2026-07-29 16:58:04 +0545

    latest update

--- extra_work/rock,paper,sicisor.py
+++ extra_work/rock,paper,sicisor.py
@@ -1,7 +1,9 @@
 import random
 lst=['rock','paper','scissior']
+
 computer= random.randint(0,2)
 human=int(input('pick the number, 0 for rock, 1 for paper and 2 for scisior:'))
+
 if human<0 or human>2:
     print('error')
 elif human==0 and computer==2:
@@ -16,5 +18,6 @@ elif human==computer:
     print('draw')
 print('human chooses:',lst[human])
 print('computer chooses:',lst[computer])
+
 
 
